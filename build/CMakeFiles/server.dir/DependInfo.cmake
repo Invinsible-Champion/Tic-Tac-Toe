6e5dac57d3aa05f7fdf05ee9b4a081775a13fc1a
Aditya Raj <rajaditya19052004@gmail.com>
2025-06-05 15:31:40 +0530

    Add files via upload

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/adi/Documents/ttt/board.cpp" "CMakeFiles/server.dir/board.cpp.o" "gcc" "CMakeFiles/server.dir/board.cpp.o.d"
+  "/home/adi/Documents/ttt/generate_moves.cpp" "CMakeFiles/server.dir/generate_moves.cpp.o" "gcc" "CMakeFiles/server.dir/generate_moves.cpp.o.d"
+  "/home/adi/Documents/ttt/main.cpp" "CMakeFiles/server.dir/main.cpp.o" "gcc" "CMakeFiles/server.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/board.cpp.o"
+  "CMakeFiles/server.dir/board.cpp.o.d"
+  "CMakeFiles/server.dir/generate_moves.cpp.o"
+  "CMakeFiles/server.dir/generate_moves.cpp.o.d"
+  "CMakeFiles/server.dir/main.cpp.o"
+  "CMakeFiles/server.dir/main.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
